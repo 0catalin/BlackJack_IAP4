907c2b012fef9e440510640fe6c038c4ba8369f3
0catalin <andrewcatalyno@gmail.com>
2024-12-13 12:45:37 +0200

    made changes

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ pygame.display.set_caption("BlackJack for everyone")
 BG = pygame.image.load("assets/Background.png")
 
 second_image = pygame.image.load("assets/Options Rect.png")
-test = True
+
 
 def get_font(size):
     return pygame.font.Font("assets/font.ttf", size)
@@ -53,18 +53,18 @@ def main_menu():
 
 
 def play():
-    global test
+
+    good_input = True
 
 
     input_text1 = ""  # This will store the user input
     input_text2 = ""
-
+    error_message_1 = ""                                   #   more than 100 decks! / input not a number!
+    error_message_2 = ""                                   #   initial balance too big! / low! / input not a number!
+    
     while True:
 
    
-
-
-
         SCREEN.blit(BG, (0, 0))
 
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
@@ -74,34 +74,38 @@ def play():
         PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 100))
 
 
+        text_surface = get_font(10).render("number of decks of cards:  total initial balance:", True, (0, 0, 0))  # Text, anti-aliasing, color
+        text_position = (425, 225)
+        SCREEN.blit(text_surface, text_position)
 
-        if test:
-            input_box1 = pygame.Rect(450, 250, 200, 50)  # x, y, width, height
-            pygame.draw.rect(SCREEN, (0, 0, 0), input_box1, 2)  # Draw the input box border
-            input_text_surface = get_font(40).render(input_text1, True, (0, 0, 0))
-            SCREEN.blit(input_text_surface, (input_box1.x + 5, input_box1.y + 5))
+        text_surface = get_font(10).render(error_message_1, True, (255, 0, 0))
+        text_position = (450, 310)
+        SCREEN.blit(text_surface, text_position)
 
-            input_box2 = pygame.Rect(700, 250, 200, 50)
-            pygame.draw.rect(SCREEN, (0, 0, 0), input_box2, 2)  # Draw the input box border
-            input_text_surface = get_font(40).render(input_text2, True, (0, 0, 0))
-            SCREEN.blit(input_text_surface, (input_box1.x + 5, input_box1.y + 5))
-        else:
-            input_box1 = pygame.Rect(450, 250, 200, 50)  # x, y, width, height
-            pygame.draw.rect(SCREEN, (255, 0, 0), input_box1, 2)  # Draw the input box border
-            input_text_surface = get_font(40).render(input_text1, True, (0, 0, 0))
-            SCREEN.blit(input_text_surface, (input_box1.x + 5, input_box1.y + 5))
+        text_surface = get_font(10).render(error_message_2, True, (255, 0, 0))
+        text_position = (700, 310)
+        SCREEN.blit(text_surface, text_position)
 
-            input_box2 = pygame.Rect(700, 250, 200, 50)
-            pygame.draw.rect(SCREEN, (255, 0, 0), input_box2, 2)  # Draw the input box border
-            input_text_surface = get_font(40).render(input_text2, True, (0, 0, 0))
-            SCREEN.blit(input_text_surface, (input_box2.x + 5, input_box2.y + 5))
-        # Input Box for user input
         
 
+        
+        input_box1 = pygame.Rect(450, 250, 200, 50)
+        input_box2 = pygame.Rect(700, 250, 200, 50)
+        if error_message_1 == "":
+            pygame.draw.rect(SCREEN, (0, 0, 0), input_box1, 2)
+        else:
+            pygame.draw.rect(SCREEN, (255, 0, 0), input_box1, 2)
+        if error_message_2 == "":
+            pygame.draw.rect(SCREEN, (0, 0, 0), input_box2, 2)
+        else:
+            pygame.draw.rect(SCREEN, (255, 0, 0), input_box2, 2)
 
+            
 
-
-
+        input_text_surface = get_font(15).render(input_text1, True, (0, 0, 0))
+        SCREEN.blit(input_text_surface, (input_box1.x + 5, input_box1.y + 5))
+        input_text_surface = get_font(15).render(input_text2, True, (0, 0, 0))
+        SCREEN.blit(input_text_surface, (input_box2.x + 5, input_box2.y + 5))
 
 
 
@@ -123,30 +127,31 @@ def play():
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
-                if not custom_validation(input_text1, input_text2):
-                        print(f"User input: {input_text1} {input_text2}") # change this, note that there are 2 input texts
-                else:
 
-                    print(f"User input: {input_text1} {input_text2}") # change this, note that there are 2 input texts
+
+                #(good_input, error_message_1, error_message_2) = custom_validation(input_text1, input_text2)
+                #if good_input:
+                #    gameplay(int(input_text1), int(input_text2))
+                (good_input1, error_message_1) = custom_validation1(input_text1)
+                (good_input2, error_message_2) = custom_validation2(input_text2)
+                if good_input1 and good_input2:
+                    gameplay(int(input_text1), int(input_text2))
+                if not good_input1:
+                    input_text1 = ""
+                if not good_input2:
+                    input_text2 = ""
 
             elif event.type == pygame.KEYDOWN and input_box1.collidepoint(PLAY_MOUSE_POS):
-                test = False
                 if event.key == pygame.K_BACKSPACE:
                     input_text1 = input_text1[:-1]
                 else:
                     input_text1 += event.unicode  # Add character to the input text
             elif event.type == pygame.KEYDOWN and input_box2.collidepoint(PLAY_MOUSE_POS):
-                test = False
 
                 if event.key == pygame.K_BACKSPACE:
                     input_text2 = input_text2[:-1]
                 else:
                     input_text2 += event.unicode
-
-
-
-
-
 
 
 
@@ -165,9 +170,31 @@ def play():
 def statistics():
     i = 0
 
+def gameplay(input_text1, input_text2):
+    print("petre can start")
 
-def custom_validation(input1, input2):
-    return True
+def custom_validation1(input1):
+    try:
+        input1 = int(input1)
+    except ValueError:
+        return False, "input not a number!"
+    if input1 < 1:
+        return False, "less than 1 deck!"
+    if input1 > 100:
+        return False, "more than 100 decks!"
+    return True, ""
+    
+def custom_validation2(input2):
+    try:
+        input2 = int(input2)
+    except ValueError:
+        return False, "input not a number!"
+    if input2 > 1000000:
+        return False, "initial balance too big!"
+    if input2 < 100:
+        return False, "initial balance too low!"
+    return True, ""
+    
 
 
 main_menu()
